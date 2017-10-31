307e6f8ae1295f1260b62a99d2f13e4041d34214
homma <daisuke.homma@gmail.com>
2017-10-31 20:27:09 +0900

    committed on 2017/10/31 20:27.

--- upload.sh
+++ upload.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+##
+## @author Daisuke Homma
+##
+## to create a repository
+##
+## create repo from github.com, click '+' menu to create new repository.
+## then run below in the project directory.
+## git init
+## git commit -m 'first commit'
+## git remote add origin https://github.com/<user>/<project>.git
+## git push -u origin master
+## having done, then run this script.
+##
+## to clone a repository
+##
+## check the repository url from the [Clone or download] button
+## git clone https://github.com/<user>/<project>.git
+## modify files and run upload.sh
+##
+
 DATE=`date '+%Y/%m/%d %H:%M'`
 MESSAGE="committed on ${DATE}."
 
